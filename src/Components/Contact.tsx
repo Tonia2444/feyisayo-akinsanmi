ff19db0824a5daa854a48ddda9b4d45661015732
tonia <feyisayoakinsanmi48@gmail.com>
2025-09-01 09:42:32 +0100

    corrected a bug

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -80,7 +80,7 @@ const Contact = () => {
         initial={{scale: 0.5, opacity: 0}}
         whileInView={{scale: 1, opacity: 1}}
         transition={{ duration: 1.5}}
-      className=" w-full mx-auto lg:px-30 md:px-15 px-5 mt-10">
+       className=" w-full mx-auto lg:px-30 md:px-15 px-5 mt-10">
         <h1 className="sm:text-4xl text-3xl text-center font-bold">Get in touch</h1>
         <div className="md:grid grid-row grid-cols-2 lg:gap-15 md:gap-10 py-15">
           <div className="flex flex-col gap-5 md:pt-15 pt-0">
@@ -102,8 +102,6 @@ const Contact = () => {
             </div>
           </div>
 
-          
-
           <div className="flex flex-col gap-5">
             <form onSubmit={onSubmit}>
               {myForm.map((form, id) => {
@@ -123,54 +121,6 @@ const Contact = () => {
           </div>
         </div>
       </motion.div>
-      <motion.div 
-        initial={{scale: 0.5, opacity: 0}}
-        whileInView={{scale: 1, opacity: 1}}
-        transition={{ duration: 1.5}}
-      className=" w-full mx-auto lg:px-30 md:px-15 px-5 mt-10">
-        <h1 className="sm:text-4xl text-3xl text-center font-bold">Get in touch</h1>
-        <div className="md:grid grid-row grid-cols-2 lg:gap-15 md:gap-10 py-15">
-          <div className="flex flex-col gap-5 md:pt-15 pt-0">
-            <div className="flex flex-col gap-5">
-              <h1 className="lg:text-4xl md:text-3xl text-2xl font-medium bg-gradient-to-r from-purple-600 to-orange-600 w-fit text-transparent bg-clip-text">Let's talk</h1>
-              <p>I'm currently available to take on new projects, so feel free to send me a message
-                about anything that you want me to work on. You can contact anytime. 
-              </p>
-            </div>
-            <div className="flex flex-col gap-3">
-              {contactMe.map((eachContact, id)=> {
-                return (
-                  <div key={id} className="flex gap-2 items-center">
-                    <eachContact.myIcon size={20} />
-                    <small>{eachContact.smallDetails}</small>
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-
-          
-
-          <div className="flex flex-col gap-5">
-            <form onSubmit={onSubmit}>
-              {myForm.map((form, id) => {
-                return (
-                  <div key={id} className="flex flex-col gap-5 mt-5">
-                    <label>{form.label}</label>
-                    <input type={form.type} name="name" placeholder={form.placeholder} required className={`bg-neutral-800 w-full py-3 px-2`}/>
-                  </div>
-                )
-              })}
-              <div className="mt-5">
-                <label className="">Message</label>
-                <textarea placeholder="Enter your message" className="bg-neutral-800 w-full px-2 pb-20 mt-5 py-2 "/>
-              </div>
-              <button type="submit"  className="px-7 mt-5 p-2 w-fit rounded-full bg-gradient-to-r from-purple-600 to-orange-600 cursor-pointer">Submit now</button>
-            </form>
-          </div>
-        </div>
-      </motion.div>
-
     </div>
   </div>;
 };
